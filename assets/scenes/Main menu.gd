bc55ac86e4e892bafba8e879c824208956d3029f
Elico Elric <theelico@gmail.com>
2021-10-21 22:18:42 +0300

    Project setup + MM
     main menu added + project setup

---
+++ assets/scenes/Main menu.gd
@@ -0,0 +1,21 @@
+extends MarginContainer
+
+var selected_menu = 0
+
+func highlight_selected():
+	pass
+
+func _ready():
+	pass
+
+func _input(event):
+	pass
+
+func _on_StartButton_pressed():
+	print("buton pressed")
+	pass
+
+
+func _on_ExitButton_pressed():
+	print("Exit buton pressed")
+	get_tree().quit()
